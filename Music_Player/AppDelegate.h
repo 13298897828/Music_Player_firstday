c0c99c4e3eb51755d43c3c494596f8f221cdeba9
Tianqi Zhang <1120404010@qq.com>
2015-10-09 15:57:48 +0800

    Initial Commit

---
+++ Music_Player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Music_Player
+//
+//  Created by 张天琦 on 15/10/9.
+//  Copyright © 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Music_Player/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Music_Player
+//
+//  Created by 张天琦 on 15/10/9.
+//  Copyright © 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
